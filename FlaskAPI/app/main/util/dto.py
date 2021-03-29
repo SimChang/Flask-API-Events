c01f5f6cb6afdb1bb1cac43c784ab29393fdcaad
Simon CHANG <simonchangpro@gmail.com>
2021-03-29 19:30:54 +0200

    [Get_event] User input for start date field not required (defaults to now)

--- FlaskAPI/app/main/util/dto.py
+++ FlaskAPI/app/main/util/dto.py
@@ -5,7 +5,7 @@ class EventDto:
     api = Namespace('event', description='event related operations')
     event = api.model('user', {
         'name': fields.String(required=True, description='event name'),
-        'start': fields.String(required=True, description='event start date'),
+        'start': fields.String(description='event start date'),
         'stop': fields.String(description='event stop date'),
         'tags': fields.List(fields.String, description='tags associated with the event')
     })
