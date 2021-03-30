026e6b0df72d0a8d0c80a1990b041e80a7c644ba
Simon CHANG <simonchangpro@gmail.com>
2021-03-30 11:52:37 +0200

    [Config] Switched to production mode

--- FlaskAPI/manage.py
+++ FlaskAPI/manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager
 from app import blueprint
 from app.main import create_app, db
 
-app = create_app('dev')
+app = create_app('prod')
 app.register_blueprint(blueprint)
 
 app.app_context().push()
